d36376c309411f62f489046c712353cc5601731d
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-02-25 11:15:57 +0100

    add Serilog

--- PetHotel/Program.cs
+++ PetHotel/Program.cs
@@ -1,5 +1,7 @@
 using PetHotel.Application.Extensions;
 using PetHotel.Infrastructure.Extensions;
+using Serilog;
+using Serilog.Events;
 
 namespace PetHotel
 {
@@ -15,21 +17,26 @@ namespace PetHotel
             builder.Services.AddInfrastructure(builder.Configuration);
             builder.Services.AddApplication();
             builder.Services.AddSwaggerGen();
+            builder.Host.UseSerilog((context, configuration) => configuration
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
+                .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}"));
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
-            if(app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
+            app.UseSerilogRequestLogging();
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
