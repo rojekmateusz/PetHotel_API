fb7a19dde5f8d134e5433e367a96c1545ee6f8bc
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-03-07 16:20:15 +0100

    add room create and update validators

---
+++ PetHotel.Application/UseCases/Room/Command/CreateRoom/CreateRoomCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System.ComponentModel.Design;
+
+namespace PetHotel.Application.UseCases.Room.Command.CreateRoom;
+
+public class CreateRoomCommandValidator : AbstractValidator<CreateRoomCommand>
+{
+    private readonly List<string> validIsActive = ["Active", "Inactive"];
+    public CreateRoomCommandValidator()
+    {
+        RuleFor(dto => dto.IsAvailable)
+            .Must(validIsActive.Contains)
+            .WithMessage("Invalid status.Please choose from the valid categories. Active or Inactive");
+    }
+}
---
+++ PetHotel.Application/UseCases/Room/Command/UpdateRoom/UpdateRoomCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace PetHotel.Application.UseCases.Room.Command.UpdateRoom;
+
+public class UpdateRoomCommandValidator : AbstractValidator<UpdateRoomCommand>
+{
+    private readonly List<string> validIsActive = ["Active", "Inactive"];
+    public UpdateRoomCommandValidator()
+    {
+        RuleFor(dto => dto.IsAvailable)
+            .Must(validIsActive.Contains)
+            .WithMessage("Invalid status.Please choose from the valid categories.Active or Inactive");
+    }
+}
