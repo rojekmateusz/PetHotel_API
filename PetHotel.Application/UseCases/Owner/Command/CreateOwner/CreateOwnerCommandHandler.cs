cdf111e0cc6c42425e52f7798fd8f5944a11df87
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-03-13 12:24:12 +0100

    add UserID to CreateOwnerCommandHandler

--- PetHotel.Application/UseCases/Owner/Command/CreateOwner/CreateOwnerCommandHandler.cs
+++ PetHotel.Application/UseCases/Owner/Command/CreateOwner/CreateOwnerCommandHandler.cs
@@ -1,16 +1,23 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using PetHotel.Application.User;
 using PetHotel.Domain.Repositories;
 
 namespace PetHotel.Application.UseCases.Owner.Command.CreateOwner;
 
-public class CreateOwnerCommandHandler(ILogger<CreateOwnerCommandHandler> logger, IMapper mapper, IOwnerRepository ownerRepository) : IRequestHandler<CreateOwnerCommand, int>
+public class CreateOwnerCommandHandler(ILogger<CreateOwnerCommandHandler> logger, IMapper mapper, IOwnerRepository ownerRepository,
+    IUserContext userContext) : 
+    IRequestHandler<CreateOwnerCommand, int>
 {
     public async Task<int> Handle(CreateOwnerCommand request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Create new Owner {@Owner}", request);
+        var currentUser = userContext.GetCurrentUser();
+        logger.LogInformation("{UserEmail} [{UserId}] is creating a new owner {@Owner}", currentUser.Email, currentUser.Id, request);
+        
         var owner = mapper.Map<Domain.Entities.Owner>(request);
+        owner.UserId = currentUser.Id;  
         int id = await ownerRepository.CreateOwner(owner);
         return id;
     }
