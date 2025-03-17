8c2bf193a56728cc7e93f9cd1ee465076b2b2b19
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-03-17 12:13:29 +0100

    add hotel pagination request validation

---
+++ PetHotel.Application/UseCases/Hotel/Queries/GetAllHotels/GetAllHotelsQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace PetHotel.Application.UseCases.Hotel.Queries.GetAllHotels;
+
+public class GetAllHotelsQueryValidator : AbstractValidator<GetAllHotelsQuery>
+{
+    private int[] allowPageSizes= { 5, 10, 20, 30 };
+
+    public GetAllHotelsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber)
+            .GreaterThanOrEqualTo(1);
+
+        RuleFor(r => r.PageSize)
+            .Must(value => allowPageSizes.Contains(value))
+            .WithMessage($"Page size must be in [{string.Join(",", allowPageSizes)}]");
+    }
+}
