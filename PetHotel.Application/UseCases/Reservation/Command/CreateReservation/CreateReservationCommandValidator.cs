26b8f455a7094f8bf2ec1a5c92b65d91f1c70d8e
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-03-20 16:47:35 +0100

    add CreateReservationCommand validator

---
+++ PetHotel.Application/UseCases/Reservation/Command/CreateReservation/CreateReservationCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace PetHotel.Application.UseCases.Reservation.Command.CreateReservation;
+
+public class CreateReservationCommandValidator: AbstractValidator<CreateReservationCommand>
+{
+    public CreateReservationCommandValidator()
+    {
+        RuleFor(dto => dto.Notes)
+            .MaximumLength(500)
+            .WithMessage("Notes must not exceed 500 characters.");
+    }
+}
