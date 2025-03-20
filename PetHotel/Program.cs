54358e19f96220cb6fc96dd95d3ff775bfbdab82
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-03-20 13:06:04 +0100

    add Serilog.Sinks.ApplicationInsights

--- PetHotel/Program.cs
+++ PetHotel/Program.cs
@@ -13,51 +13,62 @@ namespace PetHotel.API
     {
         public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-           
-            builder.Services.AddInfrastructure(builder.Configuration);
-            builder.Services.AddApplication();
-            builder.AddPresentation();
-
-            var app = builder.Build();
-
-            app.Use((context, next) =>
+            try
             {
-                context.Request.EnableBuffering();
-                return next();
-            });
+                var builder = WebApplication.CreateBuilder(args);
 
-            var scope = app.Services.CreateScope();
-            var seeder = scope.ServiceProvider.GetRequiredService<ISeeder>();
-            await seeder.Seed();
+                builder.Services.AddInfrastructure(builder.Configuration);
+                builder.Services.AddApplication();
+                builder.AddPresentation();
+
+                var app = builder.Build();
+
+                app.Use((context, next) =>
+                {
+                    context.Request.EnableBuffering();
+                    return next();
+                });
+
+                var scope = app.Services.CreateScope();
+                var seeder = scope.ServiceProvider.GetRequiredService<ISeeder>();
+                await seeder.Seed();
 
 
-            // Configure the HTTP request pipeline.
-            app.UseMiddleware<UserCreationMiddleware>();
-            
-            app.UseMiddleware<ErrorHandlingMiddleware>();
-            
-            app.UseSerilogRequestLogging();
+                // Configure the HTTP request pipeline.
+                app.UseMiddleware<UserCreationMiddleware>();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseMiddleware<ErrorHandlingMiddleware>();
+
+                app.UseSerilogRequestLogging();
+
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.MapGroup("api/identity").MapIdentityApi<User>();
+
+
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                app.Run();
             }
 
-            app.UseHttpsRedirection();
-                        
-            app.MapGroup("api/identity").MapIdentityApi<User>();
+            catch(Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
 
-            
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
         }
     }
 }
