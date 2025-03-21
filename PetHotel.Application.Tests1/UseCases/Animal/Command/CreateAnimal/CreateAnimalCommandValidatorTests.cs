a7141a6306f74f2d149e0d62f7753a786c6e5c7f
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-03-21 17:19:21 +0100

    add createAnimalCommandValidator test

---
+++ PetHotel.Application.Tests1/UseCases/Animal/Command/CreateAnimal/CreateAnimalCommandValidatorTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentValidation.TestHelper;
+using PetHotel.Application.UseCases.Animal.Command.CreateAnimal;
+using Xunit;
+
+namespace PetHotel.Application.Tests.UseCases.Animal.Command.CreateAnimal;
+
+public class CreateAnimalCommandValidatorTests
+{
+    [Fact()]
+    public void Validator_ForValidCommand_ShouldNotHaveValidationErrors()
+    {
+        // arrange
+
+        var command = new CreateAnimalCommand()
+        {
+            Name = "Pusia",
+            Age = 1,
+            Weight = 3,
+        };
+
+        var validator = new CreateAnimalCommandValidator();
+
+        // act
+
+        var result = validator.TestValidate(command);
+
+        // assert
+
+        result.ShouldNotHaveAnyValidationErrors();
+
+    }
+
+    [Fact()]
+    public void Validator_ForInValidCommand_ShouldNotHaveValidationErrors()
+    {
+        // arrange
+
+        var command = new CreateAnimalCommand()
+        {
+            Name = "P",
+            Age = 0,
+            Weight = 0
+        };
+
+        var validator = new CreateAnimalCommandValidator();
+
+        // act
+
+        var result = validator.TestValidate(command);
+
+        // assert
+
+        result.ShouldHaveValidationErrorFor(a => a.Name);
+        result.ShouldHaveValidationErrorFor(a => a.Age);
+        result.ShouldHaveValidationErrorFor(a => a.Weight);
+
+
+    }
+}
