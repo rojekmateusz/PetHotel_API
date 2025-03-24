fe4bafa12234f8e56ee3b6aa03ae069f3398727d
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-03-24 10:57:06 +0100

    add create/updateServiceCommandValidator test

---
+++ PetHotel.Application.Tests1/UseCases/Service/Command/UpdateService/UpdateServiceCommandValidatorTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation.TestHelper;
+using PetHotel.Application.UseCases.Service.Command.CreateService;
+using PetHotel.Application.UseCases.Service.Command.UpdateService;
+using Xunit;
+
+namespace PetHotel.Application.Tests.UseCases.Service.Command.UpdateService;
+
+public class UpdateServiceCommandValidatorTests
+{
+    [Fact()]
+    public void Validator_ForValidCommand_ShouldNotHaveAnyValidatorErrors()
+    {
+        // arrange
+
+        var commad = new UpdateServiceCommand
+        {
+            Description = "Some valid description"
+        };
+
+        var validator = new UpdateServiceCommandValidator();
+
+        // act
+
+        var result = validator.TestValidate(commad);
+
+        // assert
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact()]
+    public void Validator_ForInValidCommand_ShouldHaveValidatorErrors()
+    {
+        // arrange
+
+        var commad = new UpdateServiceCommand
+        {
+            Description = new string('a', 501)
+        };
+
+        var validator = new UpdateServiceCommandValidator();
+
+        // act
+
+        var result = validator.TestValidate(commad);
+
+        // assert
+
+        result.ShouldHaveValidationErrorFor(r => r.Description);
+    }
+}
