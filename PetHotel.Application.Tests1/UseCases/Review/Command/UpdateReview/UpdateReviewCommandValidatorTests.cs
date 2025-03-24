59e39c6913415a955ebd2016a2a548db94d7fd43
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-03-24 10:30:44 +0100

    add create/updateReviewCommandValidator test

---
+++ PetHotel.Application.Tests1/UseCases/Review/Command/UpdateReview/UpdateReviewCommandValidatorTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation.TestHelper;
+using PetHotel.Application.UseCases.Review.Command.CreateReview;
+using PetHotel.Application.UseCases.Review.Command.UpdateReview;
+using Xunit;
+
+namespace PetHotel.Application.Tests.UseCases.Review.Command.UpdateReview;
+
+public class UpdateReviewCommandValidatorTests
+{
+    [Fact()]
+    public void Validator_ForValidCommand_ShouldNotHaveAnyValidatorErrors()
+    {
+        // arrange
+
+        var command = new UpdateReviewCommand
+        {
+            Comment = "This is a valid comment"
+        };
+
+        var validator = new UpdateReviewCommandValidator();
+
+        // act 
+
+        var result = validator.TestValidate(command);
+
+        // assert
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact()]
+    public void Validator_ForInValidCommand_ShoulHaveValidatorErrors()
+    {
+        // arrange
+
+        var command = new UpdateReviewCommand
+        {
+            Comment = new string('a', 501)
+        };
+
+        var validator = new UpdateReviewCommandValidator();
+
+        // act 
+
+        var result = validator.TestValidate(command);
+
+        // assert
+
+        result.ShouldHaveValidationErrorFor(r => r.Comment);
+    }
+}
