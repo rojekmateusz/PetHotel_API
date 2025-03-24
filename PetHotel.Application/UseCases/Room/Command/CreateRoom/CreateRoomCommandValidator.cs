148c2d47ca4927c2a2723b761107ebff5e09c49a
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-03-24 10:49:36 +0100

    add create/updateRoomCommandValidator test

--- PetHotel.Application/UseCases/Room/Command/CreateRoom/CreateRoomCommandValidator.cs
+++ PetHotel.Application/UseCases/Room/Command/CreateRoom/CreateRoomCommandValidator.cs
@@ -5,11 +5,11 @@ namespace PetHotel.Application.UseCases.Room.Command.CreateRoom;
 
 public class CreateRoomCommandValidator : AbstractValidator<CreateRoomCommand>
 {
-    private readonly List<string> validIsActive = ["Active", "Inactive"];
+    private readonly List<string> validIsAvailable = ["Available", "Unavailable"];
     public CreateRoomCommandValidator()
     {
         RuleFor(dto => dto.IsAvailable)
-            .Must(validIsActive.Contains)
-            .WithMessage("Invalid status.Please choose from the valid categories. Active or Inactive");
+            .Must(validIsAvailable.Contains)
+            .WithMessage("Invalid status.Please choose from the valid categories. Available or Unavailable");
     }
 }
--- PetHotel.Application/UseCases/Room/Command/UpdateRoom/UpdateRoomCommandValidator.cs
+++ PetHotel.Application/UseCases/Room/Command/UpdateRoom/UpdateRoomCommandValidator.cs
@@ -4,11 +4,11 @@ namespace PetHotel.Application.UseCases.Room.Command.UpdateRoom;
 
 public class UpdateRoomCommandValidator : AbstractValidator<UpdateRoomCommand>
 {
-    private readonly List<string> validIsActive = ["Active", "Inactive"];
+    private readonly List<string> validIsActive = ["Available", "Unavailable"];
     public UpdateRoomCommandValidator()
     {
         RuleFor(dto => dto.IsAvailable)
             .Must(validIsActive.Contains)
-            .WithMessage("Invalid status.Please choose from the valid categories.Active or Inactive");
+            .WithMessage("Invalid status.Please choose from the valid categories.Available or Unavailable");
     }
 }
